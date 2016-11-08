1a3f630eadfcb3d7c28755a4c531af8058ba3783
Allen Short <ashort@mozilla.com>
2016-11-08 16:15:35 -0600

    Documentation links for data sources (re #6)

--- redash/query_runner/google_spreadsheets.py
+++ redash/query_runner/google_spreadsheets.py
@@ -106,6 +106,7 @@ def parse_worksheet(worksheet):
         columns.append({
             'name': column_name,
             'friendly_name': column_name,
+
             'type': TYPE_STRING
         })
 
@@ -138,6 +139,9 @@ class TimeoutSession(Session):
 
 
 class GoogleSpreadsheet(BaseQueryRunner):
+    default_doc_url = ("http://redash.readthedocs.io/en/latest/"
+                       "datasources.html#google-spreadsheets")
+
     @classmethod
     def annotate_query(cls):
         return False
@@ -158,6 +162,11 @@ class GoogleSpreadsheet(BaseQueryRunner):
                 'jsonKeyFile': {
                     "type": "string",
                     'title': 'JSON Key File'
+                },
+                "doc_url": {
+                    "type": "string",
+                    "title": "Documentation URL",
+                    "default": cls.default_doc_url
                 }
             },
             'required': ['jsonKeyFile'],
