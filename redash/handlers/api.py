538976131a417e3bb36950afa3d685d5ebd8b51e
Allen Short <ashort@mozilla.com>
2016-12-13 14:38:36 -0600

    basic query-version endpoint

--- redash/handlers/api.py
+++ redash/handlers/api.py
@@ -9,7 +9,10 @@ from redash.handlers.alerts import AlertResource, AlertListResource, AlertSubscr
 from redash.handlers.dashboards import DashboardListResource, RecentDashboardsResource, DashboardResource, DashboardShareResource, PublicDashboardResource 
 from redash.handlers.data_sources import DataSourceTypeListResource, DataSourceListResource, DataSourceSchemaResource, DataSourceResource, DataSourcePauseResource, DataSourceTestResource
 from redash.handlers.events import EventResource
-from redash.handlers.queries import QueryForkResource, QueryRefreshResource, QueryListResource, QueryRecentResource, QuerySearchResource, QueryResource, MyQueriesResource
+from redash.handlers.queries import (
+    MyQueriesResource, QueryForkResource, QueryListResource,
+    QueryRecentResource, QueryRefreshResource, QueryResource,
+    QuerySearchResource, QueryVersionListResource, QueryVersionResource)
 from redash.handlers.query_results import QueryResultListResource, QueryResultResource, JobResource
 from redash.handlers.users import UserResource, UserListResource, UserInviteResource, UserResetPasswordResource
 from redash.handlers.visualizations import VisualizationListResource
@@ -73,6 +76,10 @@ api.add_org_resource(MyQueriesResource, '/api/queries/my', endpoint='my_queries'
 api.add_org_resource(QueryRefreshResource, '/api/queries/<query_id>/refresh', endpoint='query_refresh')
 api.add_org_resource(QueryResource, '/api/queries/<query_id>', endpoint='query')
 api.add_org_resource(QueryForkResource, '/api/queries/<query_id>/fork', endpoint='query_fork')
+api.add_org_resource(QueryVersionListResource, '/api/queries/<query_id>/version',
+                     endpoint='query_versions')
+api.add_org_resource(QueryVersionResource, '/api/queries/<query_id>/version/<version_id>',
+                     endpoint='query_version')
 
 api.add_org_resource(ObjectPermissionsListResource, '/api/<object_type>/<object_id>/acl', endpoint='object_permissions')
 api.add_org_resource(CheckPermissionResource, '/api/<object_type>/<object_id>/acl/<access_type>', endpoint='check_permissions')
