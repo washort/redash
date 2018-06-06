53609964ed1df3f6f4c7642e35ece645b9241346
Allen Short <ashort@mozilla.com>
2018-06-06 10:00:30 -0500

    dynamic table wip

---
+++ client/app/react-components/GridRenderer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+export default class GridRenderer extends React.Component {
+  static propTypes = {
+    options: PropTypes.object.isRequired,
+    queryResult: PropTypes.object.isRequired,
+  }
+
+  render() {
+    return null;
+  }
+}
